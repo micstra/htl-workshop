924cb523236252df74e04946841191897ccf1e5c
Michael Strasser <stra@gmx.at>
2022-06-15 17:19:37 +0200

    Include gradle setup to support openapi-generation from spec

--- workshop-service/src/main/kotlin/io/cloudflight/workshop/WorkshopApplication.kt
+++ workshop-service/src/main/kotlin/io/cloudflight/workshop/WorkshopApplication.kt
@@ -3,7 +3,7 @@ package io.cloudflight.workshop
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackageClasses = [WorkshopApplication::class])
 class WorkshopApplication
 
 fun main(args: Array<String>) {
