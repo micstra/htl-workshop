0274582bc55a49d9d0e172eac2752d64bc30079b
Michael Strasser <stra@gmx.at>
2022-06-17 14:13:56 +0200

    Include openapi configuration for the vue frontend

--- workshop-ui/vite.config.ts
+++ workshop-ui/vite.config.ts
@@ -1,14 +1,23 @@
-import { fileURLToPath, URL } from 'url'
+import {fileURLToPath, URL} from 'url'
 
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+    plugins: [vue()],
+    server: {
+        proxy: {
+            "/backend": {
+                target: "http://localhost:8080/",
+                rewrite: path => path.replace("/backend", "")
+            }
+        }
+    },
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url)),
+            'workshop-client': fileURLToPath(new URL('./target', import.meta.url))
+        }
     }
-  }
 })
